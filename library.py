18e65de897a07fdf5104a34c636f9622830f4d13
Monika-Karthikeyan-24 <monika.k2024@vitstudent.ac.in>
2026-09-02 19:20:30 +0530

    Initial commit

---
+++ library.py
@@ -0,0 +1,14 @@
+print("\nLibrary Management System\n")
+
+book = "Python Programming"
+available = True
+
+if available:
+    print("Book is available")
+else:
+    print("Book is not available")
+
+if book == "Python Programming":
+    print("Book found")
+else:
+    print("Book not found")
